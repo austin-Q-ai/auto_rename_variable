d072523479fb0dcc10bf95427b7626753c6a9ee9
QsageHint <qsagehint0019@gmail.com>
2023-12-07 16:19:39 +0800

    feat: update parser

--- parser/parse.py
+++ parser/parse.py
@@ -11,12 +11,26 @@ def fix_python_code(code):
 
 class VariableVisitor(ast.NodeVisitor):
     def __init__(self):
+        super().__init__()  # Initialize the base class
         self.variables = set()
+        self.parameters = set()
 
     def visit_Assign(self, node):
         for target in node.targets:
             if isinstance(target, ast.Name):
                 self.variables.add(target.id)
+        self.generic_visit(node)
+
+    def visit_FunctionDef(self, node):
+        for arg in node.args.args:
+            # Note: In Python 3.8 and later, you can use 'arg.arg' instead of 'arg.id'
+            self.parameters.add(arg.arg)
+        # Check for arguments in other parameter lists (e.g., *args, **kwargs)
+        if node.args.vararg:
+            self.parameters.add(node.args.vararg.arg)
+        if node.args.kwarg:
+            self.parameters.add(node.args.kwarg.arg)
+        # Visit the body of the function in case it contains more variables
         self.generic_visit(node)
 
 
@@ -27,4 +41,4 @@ def parse(methodStr):
     visitor = VariableVisitor()
     visitor.visit(tree)
 
-    return list(visitor.variables)
+    return list(visitor.variables) + list(visitor.parameters)
